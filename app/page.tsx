35104bec062de30a62a2a8973232c7482caeaade
Bola Salah <103209637+BolaSalah@users.noreply.github.com>
2024-07-11 18:09:51 +0300

    structure of my app

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Nav from "@/Components/Nav";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="overflow-x-hidden font-primary">
+    </div>
+  );
+}
