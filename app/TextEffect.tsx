9ce4fba3dac98a5b313dace4224c032b0592f51a
Bola Salah <103209637+BolaSalah@users.noreply.github.com>
2024-07-12 10:24:31 +0300

    add particle and textEffect packeges

---
+++ app/TextEffect.tsx
@@ -0,0 +1,23 @@
+import { TypeAnimation } from 'react-type-animation';
+
+export  const TextEffect = () => {
+  return (
+    <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed out once, initially
+        'Front-End Developer',
+        1000, // wait 1s before replacing
+        'Ui & Ux Developer',
+        1000,
+        'Web Developer',
+        1000,
+        'Programmer',
+        1000,
+      ]}
+      wrapper='span'
+      speed={50}
+      className='lg:text-4xl md:text-2xl text-lg font-bold text-[#55e6a5] uppercase'
+      repeat={Infinity}
+    />
+  );
+};
