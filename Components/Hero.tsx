1f98c81cd3a4fb8d53ad2def9a0afb39d3d2a620
Bola Salah <103209637+BolaSalah@users.noreply.github.com>
2024-09-23 07:14:16 +0300

    Update Hero section
    replaced classes in JSX and moved all styles to global.css

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -1,40 +1,37 @@
 import React from 'react';
 import { ArrowDownTrayIcon } from '@heroicons/react/16/solid';
-import Particle from './Particle'; // to dynamic background
 import TextEffect from './TextEffect'; // to type effect
 import { ToastContainer, toast } from 'react-toastify'; // to show toast when click download cv
 import 'react-toastify/dist/ReactToastify.css';
+import Image from 'next/image';
 
 const Hero = () => {
-
   const notify = () => toast(`Preparing Download...`);
-  
+
   return (
-    <div className='p-8 sm:p-16 bg-[url("/images/banner.jpg")] bg-cover bg-center overflow-x-hidden'>
-      <Particle />
-      <div className='lg:pt-1 pt-2 grid md:grid-cols-2 grid-cols-1 justify-items-center items-center w-[80%] h-full gap-12 mx-auto'>
+    <div className='hero' id='Home'>
+      <div className='hero-main-container'>
         {/* hero-main */}
-        <div className='flex flex-col justify-center lg:items-start items-center md:order-1 order-2'>
-          <h1 className='mb-3 font-bold lg:text-4xl md:text-2xl text-lg text-white'>
-            HI, I'm <span className='text-yellow-300'>Bola Salah</span>
+        <div className='hero-main'>
+          <h1 className='hero-name'>
+            HI, I &rsquo; m <span className='text-yellow-300'>Bola Salah</span>
           </h1>
           <TextEffect />
-          <div className='text-[#ffffff92] mt-3 lg:text-left text-center'>
+          <div className='hero-description'>
             I have an experience in building responsive and user-friendly
             websites , creating engaging user interfaces. Expertise in Front-End
             Technologies: From vanilla JavaScript to cutting-edge frameworks
             with React.js
           </div>
-          <div className='grid mt-3'>
-            <button className='p-4 rounded-lg bg-[#55e6a5] hover:bg-yellow-300 uppercase
-             flex justify-center gap-2' onClick={notify} >
-              <a href='./cv/Resume.pdf' target='_blank' download>
+          <div className='hero-cv'>
+            <button className='hero-cv-download' onClick={notify}>
+              <a href='./cv/FrontEnd-Bola-Resume.pdf' target='_blank' download>
                 <p> download cv </p>
               </a>
               <ArrowDownTrayIcon className='w-6 h-6' />
             </button>
             <ToastContainer
-              position="bottom-right"
+              position='bottom-right'
               autoClose={2000}
               hideProgressBar={false}
               newestOnTop={false}
@@ -43,18 +40,18 @@ const Hero = () => {
               pauseOnFocusLoss
               draggable
               pauseOnHover
-              theme="dark"
-              />
+              theme='dark'
+            />
           </div>
         </div>
         {/* hero-image */}
-        <div
-          className='md:order-2 order-1 lg:w-[400px] lg:h-[400px] sm:w-[300px] sm:h-[300px] w-[220px] h-[220px]
-           relative flex items-center justify-center rounded-full overflow-hidden' >
-          <img
-            src='./images/3.png'
+        <div className='hero-image-container'>
+          <Image
+            width={400}
+            height={400}
+            src='/images/3.png'
             alt='bola'
-            className='rounded-full object-contain '
+            className='hero-image'
           />
         </div>
       </div>
